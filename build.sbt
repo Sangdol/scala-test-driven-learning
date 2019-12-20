eccf87ed12512a7dc473adf04688d53f75194d31
Sanghyun Lee <hammerha@gmail.com>
2019-12-20 15:38:11 +0100

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "scala-test-driven-learning"
+
+version := "0.1"
+
+scalaVersion := "2.13.1"
+
+//libraryDependencies += "org.scalactic" %% "scalactic" % "3.1.0"
+//libraryDependencies += "org.scalatest" %% "scalatest" % "3.1.0" % "test"
+libraryDependencies += "org.scalatest" % "scalatest_2.13" % "3.1.0" % "test"
---
+++ src/test/scala/SyntaxTest.scala
@@ -0,0 +1,60 @@
+import org.scalatest.FunSuite
+
+object CubeCalculator extends App {
+  def cube(x: Int) = {
+    x * x * x
+  }
+}
+
+// https://docs.scala-lang.org/getting-started/intellij-track/testing-scala-in-intellij-with-scalatest.html
+class SyntaxTest extends FunSuite {
+  test("CubeCalculator.cube") {
+    assert(CubeCalculator.cube(3) === 27)
+  }
+
+  // https://docs.scala-lang.org/cheatsheets/
+  test("Variables") {
+    var variable = 5
+    variable = 3
+
+    val constant = 3
+
+    assert(variable === constant)
+  }
+
+  test("Anonymous function") {
+    assert(((x: Int) => x * x)(3) === 9)
+
+    // underscore
+    assert((1 to 3).map(_ * 2) === Array(2, 4, 6))  // positional match
+    assert((1 to 3).reduceLeft(_ + _) === 6)  // positional match
+    assert((1 to 3).map(x => x * x) === Array(1, 4, 9))  // no underscore possible in this case
+
+    // block style
+    assert((1 to 3).map { x =>
+      val y = x * 2
+      y
+    } === Array(2, 4, 6))
+
+    // pipeline style
+    val r = (1 to 3) filter {
+      _ % 2 == 0
+    } map {
+      _ * 2
+    }
+
+    assert(r === Array(4))
+
+    // This is not possible for some reason.
+//    assert((1 to 3) filter {
+//      _ % 2 == 0
+//    } map {
+//      _ * 2
+//    } === Array(4))
+
+  }
+
+  test("Array") {
+    assert((1 to 3) === Array(1, 2, 3))
+  }
+}
