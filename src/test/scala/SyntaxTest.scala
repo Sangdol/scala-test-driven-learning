b0706b39485951f167f3d2ee2b1eeed8d97a123a
Sanghyun Lee <hammerha@gmail.com>
2020-03-28 20:21:01 +0100

    Spin off function tests / Add loop tests

--- src/test/scala/SyntaxTest.scala
+++ src/test/scala/SyntaxTest.scala
@@ -2,17 +2,8 @@ import org.scalatest.funsuite.AnyFunSuite
 import scala.math._
 import scala.util.control.Breaks._
 
-object CubeCalculator extends App {
-  def cube(x: Int) = {
-    x * x * x
-  }
-}
-
 // https://docs.scala-lang.org/getting-started/intellij-track/testing-scala-in-intellij-with-scalatest.html
 class SyntaxTest extends AnyFunSuite {
-  test("CubeCalculator.cube") {
-    assert(CubeCalculator.cube(3) === 27)
-  }
 
   // https://docs.scala-lang.org/cheatsheets/
   test("Variables") {
@@ -28,59 +19,10 @@ class SyntaxTest extends AnyFunSuite {
     assert(xmax === 100)
     assert(ymax === 100)
 
-    var a, b: String = "abc"
+    val a, b: String = "abc"
 
     assert(a.isInstanceOf[String])
     assert(b === "abc")
-  }
-
-  test("Anonymous function") {
-    assert(((x: Int) => x * x)(3) === 9)
-
-    // underscore
-    assert((1 to 3).map(_ * 2) === Array(2, 4, 6))  // positional match
-    assert((1 to 3).reduceLeft(_ / _) === 0)  // positional match
-    assert((1 to 3).map(x => x * x) === Array(1, 4, 9))  // no underscore possible in this case
-
-    // block style
-    assert((1 to 3).map { x =>
-      val y = x * 2
-      y
-    } === Array(2, 4, 6))
-
-    // pipeline style
-    val r = (1 to 3) filter {
-      _ % 2 == 0
-    } map {
-      _ * 2
-    }
-
-    assert(r === Array(4))
-
-    // This is not possible for some reason.
-//    assert((1 to 3) filter {
-//      _ % 2 == 0
-//    } map {
-//      _ * 2
-//    } === Array(4))
-
-  }
-
-  test("Function call") {
-    // The rule of thumb is that a parameterless method
-    // that doesn’t modify the object has no parentheses.
-    assert("Abc".toLowerCase == "abc")
-    assert("Abc".toLowerCase() == "abc")
-
-    // Operator overloading is possible in Scala not like Java.
-    assert(1 + 2 == 1.+(2))
-
-    val x: BigInt = 123
-    assert(x * x == 15129)
-
-    // Function as a parameter
-    // _ becomes Char.
-    assert("Abc".count(_.isUpper) == 1)
   }
 
   test("Array") {
@@ -132,7 +74,7 @@ class SyntaxTest extends AnyFunSuite {
     // Blocks return the last expression.
     val block = {
       val a = 2
-      var b = 3
+      val b = 3
       a * b
     }
 
@@ -140,7 +82,8 @@ class SyntaxTest extends AnyFunSuite {
 
     // An assignment returns a Unit value.
     // Unit is a type and the only value is '()'.
-    assert({val a = 1} == ())
+    var a = 0
+    assert({a = 1} == ())
   }
 
   test("Loops") {
@@ -179,5 +122,31 @@ class SyntaxTest extends AnyFunSuite {
     }
 
     assert(sum == 21)
+
+    // Permutation
+    // It's like a nested loop in one-line.
+    var list = List[String]()
+    for (i <- 1 to 2; j <- 1 to 2) {
+      list = list.appended(f"$i$j")
+    }
+    assert(list === List("11", "12", "21", "22"))
+
+    list = List[String]()
+    for (i <- 1 to 2; j <- 1 to 2 if i != j) {
+      list = list.appended(f"$i$j")
+    }
+    assert(list === List("12", "21"))
+
+    list = List[String]()
+    for (i <- 1 to 2; from = 3 - i; j <- from to 2) {
+      list = list.appended(f"$i$j")
+    }
+    assert(list === List("12", "21", "22"))
+
+    // Generators / for comprehension
+    val vector = for (c <- "Hello"; i <- 0 to 1) yield (c + i).toChar
+
+    assert(vector.isInstanceOf[Vector[Char]])
+    assert(vector.size == 10)
   }
 }
