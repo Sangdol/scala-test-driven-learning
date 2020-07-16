f8c4d614494ac1e2d3c5724eac8724b1db79ce3b
Sanghyun Lee <hammerha@gmail.com>
2020-07-16 19:33:02 +0200

    Rename MyList to List

--- src/test/scala/fpinscala/ch3.scala
+++ src/test/scala/fpinscala/ch3.scala
@@ -6,18 +6,18 @@ package fpinscala
 
 import org.scalatest.funsuite.AnyFunSuite
 
-sealed trait MyList[+A] // abstract class
-case object Nil extends MyList[Nothing] // singleton class
-case class Cons[+B](head: B, tail: MyList[B]) extends MyList[B]
+sealed trait List[+A] // abstract class
+case object Nil extends List[Nothing] // singleton class
+case class Cons[+B](head: B, tail: List[B]) extends List[B]
 
-object MyList {
+object List {
 
-  def sum(ints: MyList[Int]): Int = ints match {
+  def sum(ints: List[Int]): Int = ints match {
     case Nil => 0
     case Cons(x,xs) => x + sum(xs)
   }
 
-  def product(ints: MyList[Double]): Double = ints match {
+  def product(ints: List[Double]): Double = ints match {
     case Nil => 1
     case Cons(0.0, _) => 0.0
     case Cons(x, xs) => x * product(xs)
@@ -25,24 +25,24 @@ object MyList {
 
   // variadic function: it takes 0 or more arguments of type A (Seq)
   // https://www.scala-lang.org/api/current/scala/collection/immutable/Seq.html
-  def apply[A](as: A*): MyList[A] =
+  def apply[A](as: A*): List[A] =
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*)) // _*: spread
 
-  def tail[A](list: MyList[A]): MyList[A] = list match {
+  def tail[A](list: List[A]): List[A] = list match {
     case Nil => sys.error("no more element")
     case Cons(_, xs) => xs
   }
 
   @scala.annotation.tailrec
-  def drop[A](list: MyList[A], n: Int): MyList[A] = list match {
+  def drop[A](list: List[A], n: Int): List[A] = list match {
     case list if n <= 0 => list  // pattern guard
     case Nil => Nil
     case Cons(x, xs) => drop(xs, n-1)
   }
 
   @scala.annotation.tailrec
-  def dropFromBook[A](l: MyList[A], n: Int): MyList[A] =
+  def dropFromBook[A](l: List[A], n: Int): List[A] =
     if (n <= 0) l
     else l match {
       case Nil => Nil
@@ -50,12 +50,12 @@ object MyList {
     }
 
   @scala.annotation.tailrec
-  def dropWhile[A](list: MyList[A], f: A => Boolean): MyList[A] = list match {
+  def dropWhile[A](list: List[A], f: A => Boolean): List[A] = list match {
     case Cons(x, xs) if (f(x)) => dropWhile(xs, f)
     case _ => list
   }
 
-  def setHead[A](list: MyList[A], element: A): MyList[A] = list match {
+  def setHead[A](list: List[A], element: A): List[A] = list match {
     case Nil => Cons(element, Nil)
     case Cons(_, xs) => Cons(element, xs)
   }
@@ -65,38 +65,38 @@ object MyList {
 class ch3 extends AnyFunSuite {
 
   test("3.1") {
-    def matchTest(list: MyList[Int]): Int =
+    def matchTest(list: List[Int]): Int =
       list match {
         case Cons(x, Cons(2, Cons(4, _))) => x // 1
         case Nil => 42 // 2
         case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y // 3
-        case Cons(h, t) => h + MyList.sum(t) // 4
+        case Cons(h, t) => h + List.sum(t) // 4
         case _ => 101 // 5
       }
 
-    assert(matchTest(MyList(1,2,4,4,5)) == 1) // 1
+    assert(matchTest(List(1,2,4,4,5)) == 1) // 1
     assert(matchTest(Nil) == 42) // 2
-    assert(matchTest(MyList()) == 42) // 2
-    assert(matchTest(MyList(1,2,3,4,5)) == 3) // 3
-    assert(matchTest(MyList(2,3,4,4,5)) == 18) // 4
-    assert(matchTest(MyList(2,3,4,4,5,6)) == 24) // 4
+    assert(matchTest(List()) == 42) // 2
+    assert(matchTest(List(1,2,3,4,5)) == 3) // 3
+    assert(matchTest(List(2,3,4,4,5)) == 18) // 4
+    assert(matchTest(List(2,3,4,4,5,6)) == 24) // 4
   }
 
   test("3.2") {
-    assert(MyList.tail(MyList(1,2,3)) == MyList(2,3))
+    assert(List.tail(List(1,2,3)) == List(2,3))
   }
 
   test("3.3") {
-    assert(MyList.setHead(MyList(1,2), 3) == MyList(3,2))
-    assert(MyList.setHead(Nil, 3) == MyList(3))
+    assert(List.setHead(List(1,2), 3) == List(3,2))
+    assert(List.setHead(Nil, 3) == List(3))
   }
 
   test("3.4") {
-    assert(MyList.drop(MyList(1,2,3), 2) == MyList(3))
-    assert(MyList.drop(MyList(1), -1) == MyList(1))
+    assert(List.drop(List(1,2,3), 2) == List(3))
+    assert(List.drop(List(1), -1) == List(1))
   }
 
   test("3.5") {
-    assert(MyList.dropWhile(MyList(1,2,3), (x: Int) => x < 3) == MyList(3))
+    assert(List.dropWhile(List(1,2,3), (x: Int) => x < 3) == List(3))
   }
 }
