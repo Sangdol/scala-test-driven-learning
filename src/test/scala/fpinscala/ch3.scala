6b8542a501e6bfc820fca33e986336491d69d179
Sanghyun Lee <hammerha@gmail.com>
2020-07-23 22:11:26 +0200

    Add fpinscala 3.15

--- src/test/scala/fpinscala/ch3.scala
+++ src/test/scala/fpinscala/ch3.scala
@@ -125,6 +125,9 @@ object List {
 
   def append2[A](l: List[A], r: List[A]): List[A] =
     foldRight(l, r)(Cons(_, _))
+
+  def flat[A](as: List[List[A]]): List[A] =
+    foldRight(as, Nil: List[A])(append2)
 }
 
 class ch3 extends AnyFunSuite {
@@ -229,5 +232,9 @@ class ch3 extends AnyFunSuite {
   test("3.14") {
     assert(List.append(List(1), 2) == List(1,2))
     assert(List.append2(List(1), List(2)) == List(1,2))
+  }
+
+  test("3.15") {
+    assert(List.flat(List(List(1),List(2))) == List(1,2))
   }
 }
