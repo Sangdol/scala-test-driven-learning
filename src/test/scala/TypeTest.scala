0bb25dac433ab3e7632c4c0ad39579615bbdeb4c
Sanghyun Lee <hammerha@gmail.com>
2020-07-23 08:47:23 +0200

    Add getClass and classOf tests

--- src/test/scala/TypeTest.scala
+++ src/test/scala/TypeTest.scala
@@ -2,4 +2,17 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class TypeTest extends AnyFunSuite {
 
+  test("getClass") {
+    assert(Nil.getClass.toString == "class scala.collection.immutable.Nil$")
+    assert(Nil.getClass.getSimpleName == "Nil$")
+    assert(Nil.getClass.getCanonicalName == "scala.collection.immutable.Nil$")
+
+
+    assert(Int.getClass.toString == "class scala.Int$")
+  }
+
+  test("classOf") {
+    assert(classOf[Int].toString == "int")
+  }
+
 }
