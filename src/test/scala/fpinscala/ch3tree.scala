f3cd31a5dcbf8e8070773facb4ab29575afd60ba
Sanghyun Lee <hammerha@gmail.com>
2020-07-25 20:42:16 +0200

    Add fpinscala 3.29

--- src/test/scala/fpinscala/ch3tree.scala
+++ src/test/scala/fpinscala/ch3tree.scala
@@ -30,6 +30,27 @@ object Tree {
       case Leaf(v) => Leaf(f(v))
       case Branch(l, r) => Branch(map(l)(f), map(r)(f))
     }
+
+  // my initial version - type order problem
+  def fold[A,B](tree: Tree[A])(f: (B,B) => B)(g: A => B): B =
+    tree match {
+      case Leaf(v) => g(v)
+      case Branch(l, r) => f(fold(l)(f)(g), fold(r)(f)(g))
+    }
+
+  // type error
+  // Leaf(f(v)): Tree[A] => Required Branch[B]
+  // Leaf(f(v)): Branch[B] => Cannot upcast Leaf[B] to Branch[B]
+//  def foldMap[A,B](tree: Tree[A])(f: A => B): Tree[B] =
+//    fold(tree)(Branch(_, _))(v => Leaf(f(v)): Branch[B])
+
+  def fold2[A,B](tree: Tree[A])(f: A => B)(g: (B,B) => B): B = tree match {
+    case Leaf(v) => f(v)
+    case Branch(l, r) => g(fold2(l)(f)(g), fold2(r)(f)(g))
+  }
+
+  def foldMap[A,B](tree: Tree[A])(f: A => B): Tree[B] =
+    fold2(tree)(v => Leaf(f(v)): Tree[B])(Branch(_,_))
 }
 
 class ch3tree extends AnyFunSuite {
@@ -53,5 +74,19 @@ class ch3tree extends AnyFunSuite {
 
   test("3.28") {
     assert(Tree.map(t)(_ * 2) == Branch(Leaf(2), Leaf(4)))
+  }
+
+  test("3.29") {
+    // size
+    assert(Tree.fold(t)((a: Int, b: Int) => 1 + a + b)(_ => 1) == 3)
+
+    // maximum
+    assert(Tree.fold(t2)((a: Int, b: Int) => a max b)(v => v) == 100)
+
+    // depth
+    assert(Tree.fold(t2)((a: Int, b: Int) => 1 + (a max b))(_ => 1) == 3)
+
+    // map
+    assert(Tree.foldMap(t)(_ * 2) == Branch(Leaf(2), Leaf(4)))
   }
 }
