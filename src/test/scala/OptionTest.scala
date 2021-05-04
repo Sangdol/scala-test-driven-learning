4ab7734277883ea2cf5ac4392a497f0bcf9fa125
Sanghyun Lee <hammerha@gmail.com>
2021-05-04 17:07:50 +0200

    Add OptionTest

---
+++ src/test/scala/OptionTest.scala
@@ -0,0 +1,16 @@
+import org.scalatest.funsuite.AnyFunSuite
+
+class OptionTest extends AnyFunSuite {
+
+  // https://alvinalexander.com/scala/how-use-fold-scala-option-some-none-syntax/
+  test("fold and getOrElse") {
+    def f (i: Int) = i + 1
+
+    val two = Some(2)
+    val DEFAULT = 100
+
+    assert(two.fold(DEFAULT)(f) == 3)
+    assert(two.map(f).getOrElse(DEFAULT) == 3)
+  }
+
+}
