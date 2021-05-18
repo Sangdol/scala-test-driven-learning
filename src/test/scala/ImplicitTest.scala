82199fcb354e25d686eefc2fd41bce026da76d39
Sanghyun Lee <hammerha@gmail.com>
2021-05-18 22:03:56 +0200

    Add more implicit tests

--- src/test/scala/ImplicitTest.scala
+++ src/test/scala/ImplicitTest.scala
@@ -35,4 +35,28 @@ class ImplicitTest extends  AnyFunSuite {
     assert(ImplicitObject.sum(List(1, 2, 3)) == 6)
   }
 
+  test("Implicit variable") {
+    implicit val rate = 0.5F
+
+    def calcTax(amount: Float)(implicit rate: Float): Float = amount * rate
+
+    assert(calcTax(10) == 5.0F)
+  }
+
+  test("Implicitly") {
+    import math.Ordering
+
+    case class MyList[A](list: List[A]) {
+      def sortBy1[B](f: A => B)(implicit ord: Ordering[B]): List[A] =
+        list.sortBy(f)(ord)
+
+      // We need 'implicitly' since Ordering is not explicitly declared.
+      def sortBy2[B : Ordering](f: A => B): List[A] =
+        list.sortBy(f)(implicitly[Ordering[B]])
+    }
+
+    assert(MyList(List(1, 3, 2)).sortBy1(i => i) == List(1, 2, 3))
+    assert(MyList(List(1, 3, 2)).sortBy2(i => i) == List(1, 2, 3))
+  }
+
 }
