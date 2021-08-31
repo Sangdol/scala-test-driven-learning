a4b2efe7a14d54714d4b16b1b480a9c2fdaaed38
Sanghyun Lee <hammerha@gmail.com>
2021-08-31 13:33:47 +0200

    Add more enum test

--- src/test/scala/EnumerationTest.scala
+++ src/test/scala/EnumerationTest.scala
@@ -11,4 +11,27 @@ class EnumerationTest extends AnyFunSuite {
     assert(Color.RED.toString == "red")
   }
 
+  test("case class") {
+
+    object Multiple extends Enumeration {
+      // Why private?
+      final case class MultipleVal private (mid: String, name: String) extends super.Val(mid)
+
+      type Multiple = MultipleVal
+
+      val sang: Multiple.MultipleVal = MultipleVal("1", "sang")
+      val hj: Multiple.MultipleVal = MultipleVal("2", "hj")
+    }
+
+    val ids = Multiple.values.map(_.toString)
+    assert(ids == Set("1", "2"))
+
+    val collectedIds = Multiple.values.collect {
+      case Multiple.MultipleVal(id, _) => id
+    }
+    assert(collectedIds == Set("1", "2"))
+
+    assert(Multiple.withName("1") == Multiple.sang)
+    assert(Multiple.withName("1").asInstanceOf[Multiple.MultipleVal] == Multiple.sang)
+  }
 }
