0e80af543fc33ff01ec54a0edf9be932aeb94e19
Sanghyun Lee <hammerha@gmail.com>
2021-11-17 10:36:19 +0100

    Add Duration.between() tests

--- src/test/scala/TimeTest.scala
+++ src/test/scala/TimeTest.scala
@@ -1,11 +1,19 @@
 import org.scalatest.funsuite.AnyFunSuite
 
-import java.time.Instant
+import java.time.{Duration, Instant}
 
 class TimeTest extends AnyFunSuite {
   test("Instant toString") {
     val i = Instant.EPOCH
 
     assert(i.toString == "1970-01-01T00:00:00Z")
+  }
+
+  test("Instant calculation") {
+    // https://stackoverflow.com/a/55780162/524588
+    val instant1 = Instant.parse("2019-02-14T18:42:00Z");
+    val instant2 = Instant.parse("2019-04-21T05:25:00Z");
+
+    assert(Duration.between(instant1, instant2).toDays == 65)
   }
 }
