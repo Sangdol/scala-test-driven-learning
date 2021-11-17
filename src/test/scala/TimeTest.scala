8ed9a5b20d0ee6f4be6f6294dee9057d629f5204
Sanghyun Lee <hammerha@gmail.com>
2021-11-17 12:05:40 +0100

    Add more time Duration test

--- src/test/scala/TimeTest.scala
+++ src/test/scala/TimeTest.scala
@@ -12,8 +12,8 @@ class TimeTest extends AnyFunSuite {
 
   test("Instant calculation") {
     // https://stackoverflow.com/a/55780162/524588
-    val instant1 = Instant.parse("2019-02-14T18:42:00Z");
-    val instant2 = Instant.parse("2019-04-21T05:25:00Z");
+    val instant1 = Instant.parse("2019-02-14T18:42:00Z")
+    val instant2 = Instant.parse("2019-04-21T05:25:00Z")
 
     assert(Duration.between(instant1, instant2).toDays == 65)
 
@@ -21,5 +21,8 @@ class TimeTest extends AnyFunSuite {
     val instant4 = Instant.EPOCH.plus(1, ChronoUnit.DAYS)
 
     assert(Duration.between(instant3, instant4).toDays == 1)
+
+    val instant5 = Instant.parse("2021-08-14T12:35:49.713Z")
+    assert(Duration.between(Instant.EPOCH, instant5).toDays == 18853)
   }
 }
