8503e444507ff7150a76c1843daed7dad6445e37
Sanghyun Lee <hammerha@gmail.com>
2022-01-14 16:58:27 +0100

    Add more option tests (None, flatten)

--- src/test/scala/ForComprehensionTest.scala
+++ src/test/scala/ForComprehensionTest.scala
@@ -121,6 +121,32 @@ class ForComprehensionTest extends AnyFunSuite {
 
     assert(s2 == Some(3))
 
+    val some3: Option[Int] = None
+
+    // None makes all result None
+    val sNone = for {
+      s1 <- some
+      s2 <- some2
+      s3 <- some3
+    } yield s1 + s2 + s3
+
+    assert(sNone == None)
+
+    val s2None = some.flatMap(s1 => some2.flatMap(s2 => some3 map (s3 => s1 + s2 + s3)))
+
+    assert(s2None == None)
+
+    // flatten
+    val ss = Seq(some, some2, some3)
+
+    assert(ss.flatten == Seq(1, 2))
+    assert(ss.flatten.sum == 3)
+
+    // flatten is the same as
+    assert(ss.flatMap(s => s) == Seq(1, 2))
+  }
+
+  test("option pattern matching") {
     // pattern matching
 
     val results: Seq[Option[Int]] = Vector(Some(10), None, Some(20))
