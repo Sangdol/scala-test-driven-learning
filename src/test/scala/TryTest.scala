dc354baf73a738a6d197d55972bb836eac291145
Sanghyun Lee <hammerha@gmail.com>
2022-01-21 14:56:30 +0100

    Add Try tests

---
+++ src/test/scala/TryTest.scala
@@ -0,0 +1,45 @@
+import org.scalatest.funsuite.AnyFunSuite
+
+import scala.util._
+
+class TryTest extends AnyFunSuite {
+  test("constructors") {
+    val s1 = Success(1)
+    val s2 = Try(1)
+
+    assert(s1 == s2)
+
+    // Call by name
+    val f1 = Try(1/0)
+    assert(f1.isFailure)
+
+    // call by value
+    val f2 = Failure(new Exception())
+    assert(f1.isFailure)
+  }
+
+  test("pattern matching") {
+    val msg = Try(1/0) match {
+      case Success(x) => "no way"
+      case Failure(e) => e.getMessage
+    }
+
+    assert(msg == "/ by zero")
+  }
+
+  test("toEither") {
+    val e = Try(1/0).toEither
+
+    assert(e.isLeft)
+  }
+
+  test("for-comprehension") {
+    val e = for {
+      t1 <- Try(0)
+      t2 <- Try(1/t1)
+    } yield t2
+
+    assert(e.isFailure)
+  }
+
+}
