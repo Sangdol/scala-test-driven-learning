8382c4cd62d1c9a3c2790c46ab3624d257d96f55
Sanghyun Lee <hammerha@gmail.com>
2022-02-17 21:55:40 +0100

    FutureMain: Use custom method instead of main

--- src/main/scala/com/iamsang/future/FutureMain.scala
+++ src/main/scala/com/iamsang/future/FutureMain.scala
@@ -9,14 +9,19 @@ import scala.util._
   * No `main` is needed with `App`.
   * https://www.scala-lang.org/api/2.13.3/scala/App.html
   */
-object FutureMain extends App {
-  println("hallo")
+object FutureMain {
 
-  val ff = Future.failed(new Exception())
-  val f1 = Future { println(1); 1 }
-  val f2 = Future { println(2); 2 }
+  def printAndThrowException(): Unit = {
+    println("hallo")
 
-  Await.result(Future.sequence(Seq(ff, f1, f2)), 1.second)
+    val ff = Future.failed(new Exception())
+    val f1 = Future { println(1); 1 }
+    val f2 = Future { println(2); 2 }
 
-  println("bis bald")
+    // This runs all futures but throws an exception.
+    Await.result(Future.sequence(Seq(ff, f1, f2)), 1.second)
+
+    println("bis bald")
+  }
+
 }
