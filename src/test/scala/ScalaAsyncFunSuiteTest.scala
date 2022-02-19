5cf42bd6478d473251f0eca885bdef3be3eb92bc
Sanghyun Lee <hammerha@gmail.com>
2022-02-19 21:22:51 +0100

    Add ScalaAsyncFunSuiteTest

---
+++ src/test/scala/ScalaAsyncFunSuiteTest.scala
@@ -0,0 +1,23 @@
+import org.scalatest.funsuite.AsyncFunSuite
+
+import scala.concurrent.Future
+
+/**
+  * Asynchronous testing
+  * - https://www.scalatest.org/user_guide/async_testing
+  */
+class ScalaAsyncFunSuiteTest extends AsyncFunSuite {
+
+  test("map assert") {
+    Future { 1 } map { n => assert(n == 1) }
+  }
+
+  test("recoverToSucceededIf / assertThrows") {
+    recoverToSucceededIf[NoSuchElementException] {
+      Future { 5 } collect {
+        case x if x > 5 => -x
+      }
+    }
+  }
+
+}
